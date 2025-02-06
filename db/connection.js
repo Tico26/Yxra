05a70965790b6378db2b0c20d9d4ae053be9c782
Bruno Jauad <brunojauad26@icloud.com>
2025-02-06 13:22:01 +0000

    configured env variables

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,16 @@
 const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+const config = {};
 
-const db = new Pool();
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
-module.exports = db;
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE not set or DATABASE_URL not set");
+}
+module.exports = new Pool(config);
